98f0936c7395222b60a9f35deaa58325b1b14b6e
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-08 21:25:05 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jQuery Event Listeners
+(# jQuery Event Listeners
 
 ## Objectives
 
--- js/events.js
+++ js/events.js
@@ -3,5 +3,33 @@
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt(){
+  $('p').on("click", function() {
+  alert('Hey');
+});
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown",function(e){
+    if(e.which === 71){
+      alert("g was pressed");
+    }
+  }
+  )
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  }
+    )
+}
+
