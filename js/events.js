b15d298fead058312394faf64acc22f9b5470b46
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-08 21:13:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,17 @@
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt(){
+  $('p').on("click", function() {
+  alert('Hey');
+});
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
